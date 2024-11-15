08beb66e9446e199c10cdc384cd4676f823baae8
Vis7044 <singhvishal7044@gmail.com>
2024-11-15 18:56:19 +0530

    Add Navbar

---
+++ src/theme.js
@@ -0,0 +1,39 @@
+
+export const darkTheme = {
+    colors: {
+        primary: "#124E66",       // Dark Blue
+        secondary: "#748D92",     // Medium Light
+        background: "#2E3944",    // Dark
+        lightText: "#ffffff",     // Light
+        darkText: "#212A31",      // Black
+        accent: "#FF6F61",        // Accent Color
+    },
+    gradients: {
+        navbar: "linear-gradient(90deg, #05080b 0%, #16241e 40%, #05080b 100%)", // Navbar gradient
+    },
+    button: {
+        background: "#1E2422",    // Button Background
+        color: "white",           // Button Text
+    },
+};
+
+
+
+export const lightTheme = {
+    colors: {
+        primary: "#4285F4",       // Bright Blue
+        secondary: "#8AB4F8",     // Soft Blue
+        background: "#FFFFFF",    // White
+        lightText: "#202124",     // Dark Text
+        darkText: "#000000",      // Black
+        accent: "#FF7043",        // Bright Orange
+    },
+    gradients: {
+        navbar: "linear-gradient(90deg, #8AB4F8, #FFFFFF)", // Navbar gradient
+        button: "linear-gradient(45deg, #4285F4, #FF7043)", // Button gradient
+    },
+    button: {
+        background: "#E3F2FD",    // Light Blue Background
+        color: "#202124",         // Dark Text
+    },
+};
