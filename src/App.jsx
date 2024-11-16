fb5fd610a49f045b91d13d46c4e00bd235b1f9f2
Sahil Sajar <sahilsajar13890@gmail.com>
2024-11-16 21:22:32 +0530

    Routes Made

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
-import {darkTheme,lightTheme} from './theme';
+import { darkTheme, lightTheme } from "./theme";
 import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -13,15 +15,14 @@ function App() {
 
   return (
     <>
-      <ThemeProvider theme={isDarkMode?darkTheme:lightTheme}>
-        <Navbar toggleTheme={toggleTheme}/>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+        <Navbar toggleTheme={toggleTheme} />
       </ThemeProvider>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </>
   );
 }
 
 export default App;
-
-
-
-
