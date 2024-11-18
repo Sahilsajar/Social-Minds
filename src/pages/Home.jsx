d402cc8148350e07674f0cab912f5950ee17d8c0
Mithalesh Kumar Bind <mk2018me@gmail.com>
2024-11-19 04:37:39 +0530

    ReviewCard1 added in components directory

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Introduction from "../components/Introduction";
+//import Try from "../components/Try";
+import ReviewCard1 from "../components/ReviewCard1"
 
 function Home() {
   return (
     <div>
       <Introduction></Introduction>
+      <ReviewCard1></ReviewCard1>
     </div>
   );
 }
