7a19632107487c3095b923983246d79ff6904ff0
Vis7044 <singhvishal7044@gmail.com>
2024-12-09 22:59:00 +0530

    minor change

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import animationData from '../../public/astronaut.json'; 
+import animationData from '../assets/astronaut.json'; 
 
 const NotFoundPage = () => {
   // Lottie animation options
